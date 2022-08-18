c886cedec6e66087ed1148260e964a9a58ef4c07
Kayhan Latifzadeh <lkyhan@gmail.com>
2022-08-18 17:33:26 +0200

    sampling arg added

--- tracker.py
+++ tracker.py
@@ -1,15 +1,30 @@
-import pyautogui
+# Load std libs.
 import time
+import argparse
 
+parser = argparse.ArgumentParser(description='Record mouse cursor position in real-time',
+  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--sampling_rate', default=1000, type=int, help='sampling rate to record cursor position')
+
+
+args = parser.parse_args()
+
+# Load 3rd party libs.
+import pyautogui
 
 # timestamp of the start of recording is a part of the filename
 ts = time.time()
 file_name = "LOG_" + str(ts) + ".csv"
 f = open(file_name, 'w')
 
+# writing the header row
 f.write('time_stamp\tx\ty\n')
 
 while True:
     currentMouseX, currentMouseY = pyautogui.position()
     ts = time.time()
-    f.write("{ts}\t{x}\t{y}\n".format(ts = ts, x = currentMouseX, y = currentMouseY))
+    sample = "{ts}\t{x}\t{y}\n".format(ts = ts, x = currentMouseX, y = currentMouseY)
+    f.write(sample)
+    time.sleep(1/args.sampling_rate)
+    
