302fa4990457928bcc58706be87ae70adbac8ca1
Kayhan Latifzadeh <lkyhan@gmail.com>
2022-08-19 07:19:22 +0200

    minor changes

--- tracker.py
+++ tracker.py
@@ -18,34 +18,37 @@ import time
 import argparse
 import os
 
+
 parser = argparse.ArgumentParser(description='Record mouse cursor position in real-time',
   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('--sampling_rate', default=60, type=int, help='sampling rate to record cursor position')
 
-
 args = parser.parse_args()
+
 
 # Load 3rd party libs.
 import pyautogui
 
+
 # timestamp of the start of recording is a part of the filename
 ts = time.time()
 file_name = "LOG_" + str(ts) + ".csv"
-f = open(file_name, 'w')
+log_file = open(file_name, 'w')
 
 # writing the header row
-f.write('time_stamp\tx\ty\n')
+log_file.write('time_stamp\tx\ty\n')
 
 try:
+    print("Mouse cursor recording started. Press ctrl+c to stop recording!\n")
     while True:
         currentMouseX, currentMouseY = pyautogui.position()
         ts = time.time()
         sample = "{ts}\t{x}\t{y}\n".format(ts = ts, x = currentMouseX, y = currentMouseY)
-        f.write(sample)
+        log_file.write(sample)
         time.sleep(1/args.sampling_rate)
 except KeyboardInterrupt:
-    f.close()
-    print("\nThe data has been saved, path to the file:", os.path.realpath(f.name))
+    log_file.close()
+    print("\nThe data has been saved, path to the file:\n", os.path.realpath(log_file.name))
     exit(0)
     
