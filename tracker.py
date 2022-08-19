b10f45cbd3e4c1fa0619e728e970b81a72c1b0b2
Kayhan Latifzadeh <lkyhan@gmail.com>
2022-08-19 07:15:03 +0200

    stop by ctrl-c added

--- tracker.py
+++ tracker.py
@@ -16,11 +16,12 @@ Authors:
 # Load std libs.
 import time
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='Record mouse cursor position in real-time',
   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('--sampling_rate', default=1000, type=int, help='sampling rate to record cursor position')
+parser.add_argument('--sampling_rate', default=60, type=int, help='sampling rate to record cursor position')
 
 
 args = parser.parse_args()
@@ -36,10 +37,15 @@ f = open(file_name, 'w')
 # writing the header row
 f.write('time_stamp\tx\ty\n')
 
-while True:
-    currentMouseX, currentMouseY = pyautogui.position()
-    ts = time.time()
-    sample = "{ts}\t{x}\t{y}\n".format(ts = ts, x = currentMouseX, y = currentMouseY)
-    f.write(sample)
-    time.sleep(1/args.sampling_rate)
+try:
+    while True:
+        currentMouseX, currentMouseY = pyautogui.position()
+        ts = time.time()
+        sample = "{ts}\t{x}\t{y}\n".format(ts = ts, x = currentMouseX, y = currentMouseY)
+        f.write(sample)
+        time.sleep(1/args.sampling_rate)
+except KeyboardInterrupt:
+    f.close()
+    print("\nThe data has been saved, path to the file:", os.path.realpath(f.name))
+    exit(0)
     
